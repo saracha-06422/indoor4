e775e82272cf9aee31434998d7590694f6ec0c96
saracha <saracha06422@gmail.com>
2020-12-17 18:53:00 +0700

    sol css

--- indoor4/settings.py
+++ indoor4/settings.py
@@ -119,8 +119,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/assets/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-    # os.path.join(BASE_DIR,'assets'),
-    'Desktop/4th/indoor4/indoor4/assets',
+    os.path.join(BASE_DIR,'static'),
+    # 'Desktop/4th/indoor4/indoor4/assets',
 )
